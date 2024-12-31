5eb7ac5958e0c218b53309a6293040bbeea9dea1
Czer0C <trieunguyen04239@gmail.com>
2024-12-31 15:35:30 +0700

    curd

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,22 +1,89 @@
+import { z } from 'astro/zod';
 import { defineAction } from 'astro:actions';
 
 const TOKEN = 'zenzeIsMoe';
 
+const HOST = 'http://34.143.206.52';
+
 export const server = {
   // action declarations
 
-  getData: defineAction({
-    // input: z.object({
-    //   name: z.string(),
-    // }),
+  createData: defineAction({
+    input: z.object({
+      url: z.string(),
+    }),
+    handler: async ({ url }) => {
+      const response = await fetch(`${HOST}/slash`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${TOKEN}`,
+        },
+        body: JSON.stringify({ url }),
+      });
+      return response.json();
+    },
+  }),
+
+  readData: defineAction({
     handler: async () => {
-      const data = await fetch('http://34.143.206.52/summary', {
+      const response = await fetch(`${HOST}/links`, {
         headers: {
           Authorization: `Bearer ${TOKEN}`,
         },
-      }).then((response) => response.json());
-
-      return data;
+      });
+      return response.json();
     },
   }),
+
+  updateData: defineAction({
+    input: z.object({
+      id: z.string(),
+      url: z.string(),
+    }),
+    handler: async ({ id, url }) => {
+      const response = await fetch(`${HOST}/slash/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${TOKEN}`,
+        },
+        body: JSON.stringify({ url }),
+      });
+      return response.json();
+    },
+  }),
+
+  deleteData: defineAction({
+    input: z.object({
+      short_code: z.string(),
+    }),
+    handler: async ({ short_code }) => {
+      const url = `${HOST}/slash/${short_code}`;
+
+      const response = await fetch(url, {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      });
+
+      return response.json();
+    },
+  }),
+
+  // getData: defineAction({
+  //   // input: z.object({
+  //   //   name: z.string(),
+  //   // }),
+  //   handler: async () => {
+  //     const data = await fetch('http://34.143.206.52/summary', {
+  //       headers: {
+  //         Authorization: `Bearer ${TOKEN}`,
+  //       },
+  //     }).then((response) => response.json());
+
+  //     return data;
+  //   },
+  // }),
 };
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,24 +1,30 @@
+import { Button } from '@/components/ui/button';
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
+import { Trash2 } from 'lucide-react';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { actions } from 'astro:actions';
 
 export function TableDemo({ data }) {
   return (
     <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
       <TableHeader>
         <TableRow>
           <TableHead className="w-[100px] font-bold">Short Code</TableHead>
           <TableHead className="">Origin</TableHead>
           <TableHead className="text-right">Created At</TableHead>
           <TableHead className="text-right">Updated At</TableHead>
+          <TableHead className=""></TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
@@ -26,17 +32,53 @@ export function TableDemo({ data }) {
           <TableRow key={i.id}>
             <TableCell className="font-bold">{i.short_code}</TableCell>
             <TableCell className="">{i.origin}</TableCell>
-            <TableCell className="text-right">{i.created_at}</TableCell>
-            <TableCell className="text-right">{i.updated_at}</TableCell>
+            <TableCell className="text-right">
+              {new Date(i.created_at).toLocaleString()}
+            </TableCell>
+            <TableCell className="text-right">
+              {new Date(i.updated_at).toLocaleString()}
+            </TableCell>
+            <TableCell className="text-right">
+              <Popover>
+                <PopoverTrigger className="flex items-center">
+                  <Trash2 />
+                </PopoverTrigger>
+                <PopoverContent>
+                  <div>
+                    Are you sure to delete short code for{' '}
+                    <b className="text-sky-500">{i.origin}</b>?
+                  </div>
+
+                  <Button
+                    variant="destructive"
+                    onClick={async () => {
+                      const { data, error } = await actions.deleteData({
+                        short_code: i.short_code,
+                      });
+
+                      if (data) {
+                        console.log(data);
+                      } else {
+                        alert(error?.message);
+                      }
+
+                      window.location.reload();
+                    }}
+                  >
+                    Confirm
+                  </Button>
+                </PopoverContent>
+              </Popover>
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
-      <TableFooter>
+      {/* <TableFooter>
         <TableRow>
           <TableCell colSpan={3}>Total</TableCell>
           <TableCell className="text-right">$2,500.00</TableCell>
         </TableRow>
-      </TableFooter>
+      </TableFooter> */}
     </Table>
   );
 }
